5c11b765b3a832560db1ca08992f39646e89adb7
k_r_a_s_h_ <krishna05564@gmail.com>
2019-03-09 16:25:47 +0530

    basic_udp fixed

--- lab5/basic_udp/server.c
+++ lab5/basic_udp/server.c
@@ -38,8 +38,10 @@ int main(){
 
     SA_IN clientaddr,serveraddr;
     init_SA(&serveraddr,AF_INET,htons(PORT),htonl(INADDR_ANY));
+    init_SA(&clientaddr,AF_INET,htons(PORT+1),htonl(INADDR_ANY));
 
-    int bind_status = bind(sockfd,(const SA*)&serveraddr,sizeof(serveraddr));
+
+    int bind_status = bind(sockfd,(const SA*)&clientaddr,sizeof(clientaddr));
     if(bind_status<0) ERROR_CLOSE(sockfd);
     else
     {
@@ -48,7 +50,7 @@ int main(){
     
 
     int len=sizeof(clientaddr);
-    int recv_status=recvfrom(sockfd,buffer,sizeof(buffer),0,(SA*)&clientaddr,&len);
+    int recv_status=recvfrom(sockfd,buffer,sizeof(buffer),0,(SA*)&serveraddr,&len);
     if(recv_status<0){
         printf("%d",recv_status);
         ERROR_CLOSE(sockfd);
@@ -64,7 +66,7 @@ int main(){
     printf("enter the message\n");
     scanf("%s",buffer_send);
 
-    int snd_status=sendto(sockfd,(const char*)buffer_send,strlen(buffer_send),0,(const SA*)&clientaddr,len);
+    int snd_status=sendto(sockfd,(const char*)buffer_send,strlen(buffer_send),0,(const SA*)&serveraddr,len);
     if(snd_status<0) ERROR_CLOSE(sockfd);
     else printf("message sent");
     close(sockfd);
